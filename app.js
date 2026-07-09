b89088992f3799afa45a3262e87a2aab7b3a631f
mdszhossain <personalmailshazzad@gmail.com>
2026-07-09 23:24:00 +0600

    root and /students route working perfectly. fetching data from database and sending it to client side.

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Student = require("./models/listing");
 const app = express();
 const port = 8080;
+
+// using middlewares
+
+// database connection
+const MONGO_URL = "mongodb://127.0.0.1:27017/bubt";
+main()
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("Error Appeared: ", err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
 
 // root route
 app.get("/", (req, res) => {
   console.log("Hello, This is root route");
-  res.send("This is root route");
+  res.redirect("/students");
+});
+
+app.get("/students", async(req, res) => {
+  const allStudents = await Student.find({});
+  res.send(allStudents);
 });
 
 app.listen(port, () => {
